4c18fbf4b40c9e9be8a94188a4a44bb788ecc4f9
G1-H <hwangjiwon1992@gmail.com>
2023-08-15 14:26:16 +0900

    Feat: Order Entity 추가
    Order Entity 추가하였습니다.

---
+++ commerce03/src/main/java/com/supercoding/commerce03/repository/order/entity/Order.java
@@ -0,0 +1,47 @@
+package com.supercoding.commerce03.repository.order.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name="Orders")
+@EqualsAndHashCode(of="order_id")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="order_id", nullable = false)
+    private Integer id;
+
+    @Column(name="user_id", nullable = false)
+    private Integer userId;
+
+    @Column(name="status", nullable = false)
+    private Integer status;
+
+    @Column(name="ordered_at", nullable = false)
+    private LocalDateTime orderedAt;
+
+    @Column(name = "address", nullable = false, length= 100)
+    private String address;
+
+    @Column(name = "phone_number", nullable = false, length = 11)
+    private String phoneNumber;
+
+    @Column(name="is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+}
