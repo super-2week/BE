4b62d00be078b9ec7b63144661601bf2df7c76f3
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-15 13:39:23 +0900

    Feat : Review 관련 Entity 생성 Review, ReviewImages entity 생성

---
+++ commerce03/src/main/java/com/supercoding/commerce03/repository/review/entity/ReviewImage.java
@@ -0,0 +1,36 @@
+package com.supercoding.commerce03.repository.review.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Review_Images")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_image_id", nullable = false)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "review_id")
+	private Review review;
+
+	@Column(name = "review_image_url")
+	private String reviewImageUrl;
+}
--- commerce03/src/main/java/com/supercoding/commerce03/repository/review/entity/test.java
+++
@@ -1,5 +0,0 @@
-package com.supercoding.commerce03.repository.review.entity;
-
-public class test {
-
-}
