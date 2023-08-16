9411445e66fcaa695bd80331185a0756325b5660
G1-H <hwangjiwon1992@gmail.com>
2023-08-17 01:38:12 +0900

    Refactor :  엔티티 수정
    Order 엔티티 어노테이션 추가 등

--- commerce03/src/main/java/com/supercoding/commerce03/repository/order/entity/Order.java
+++ commerce03/src/main/java/com/supercoding/commerce03/repository/order/entity/Order.java
@@ -3,6 +3,8 @@ package com.supercoding.commerce03.repository.order.entity;
 
 import com.supercoding.commerce03.repository.user.entity.User;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 @Builder
 @Table(name="Orders")
 @EqualsAndHashCode(of="order_id")
+@ToString
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,8 +29,9 @@ public class Order {
     private User user;
 
     @Column(name="status", nullable = false)
-    private Integer status;
+    private String status;
 
+    @CreatedDate
     @Column(name="ordered_at", nullable = false)
     private LocalDateTime orderedAt;
 
@@ -43,9 +47,12 @@ public class Order {
     @Column(name="post_comment")
     private String postComment;
 
+    @ColumnDefault("false")
     @Column(name="is_deleted", nullable = false)
     private Boolean isDeleted;
 
+    //orderedAt 이랑 중복되는 느낌 고려해보자.
+    @CreatedDate
     @Column(name="created_at", nullable = false)
     private LocalDateTime createdAt;
 
