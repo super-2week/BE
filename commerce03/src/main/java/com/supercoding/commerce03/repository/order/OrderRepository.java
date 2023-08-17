8b11cf64a3ee1e185dcce343bd7efdf056f9ad0c
G1-H <hwangjiwon1992@gmail.com>
2023-08-17 16:31:34 +0900

    Feat : 주문 목록 가져오기 repository JPA 생성
    User,Pageable 로 User에 해당하는 Order들을 가져옴.

--- commerce03/src/main/java/com/supercoding/commerce03/repository/order/OrderRepository.java
+++ commerce03/src/main/java/com/supercoding/commerce03/repository/order/OrderRepository.java
@@ -1,8 +1,17 @@
 package com.supercoding.commerce03.repository.order;
 
 import com.supercoding.commerce03.repository.order.entity.Order;
+import com.supercoding.commerce03.repository.user.entity.User;
+import com.supercoding.commerce03.web.dto.order.OrderDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Page<Order>> findAllByUser(User user, Pageable pageable);
 
 }
