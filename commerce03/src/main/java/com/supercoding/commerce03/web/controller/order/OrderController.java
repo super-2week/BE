d3a62d9b0581f38e35076600890dd1b3f522cb06
G1-H <hwangjiwon1992@gmail.com>
2023-08-17 17:26:34 +0900

    Feat : 주문 내역 삭제 컨트롤러
    주문 목록에서 주문 내역 삭제하기

--- commerce03/src/main/java/com/supercoding/commerce03/web/controller/order/OrderController.java
+++ commerce03/src/main/java/com/supercoding/commerce03/web/controller/order/OrderController.java
@@ -1,6 +1,8 @@
 package com.supercoding.commerce03.web.controller.order;
 
 import com.supercoding.commerce03.service.order.OrderService;
+//import com.supercoding.commerce03.service.security.Auth;
+//import com.supercoding.commerce03.service.security.AuthHolder;
 import com.supercoding.commerce03.web.dto.order.OrderDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,12 +29,14 @@ public class OrderController {
                 = orderService.orderRegister(userId, orderRegisterRequest);
         return ResponseEntity.ok(orderResponse);
     }
-
+    //주문 취소
+//    @Auth
     @DeleteMapping("/{orderId}")
     public ResponseEntity<?> orderCancel(
             @PathVariable String orderId
     ) {
         //TODO : 임시 유저 정보 실제 정보 받아와서 바꿔야함.
+        //        Long userId = AuthHolder.getUserId();
         String userId = "1";
         OrderDto.OrderCancelResponse orderCancelResponse
                 = orderService.orderCancel(userId, orderId);
@@ -40,6 +44,7 @@ public class OrderController {
         return ResponseEntity.ok(orderCancelResponse);
     }
 
+    //주문 목록 가져오기 페이지네이션 처리
     @GetMapping("/list")
     public ResponseEntity<?> orderList(
             Pageable pageable
@@ -51,5 +56,15 @@ public class OrderController {
                 = orderService.orderList(userId,pageable);
 
         return ResponseEntity.ok(orderListResponsePage);
+    }
+
+    //주문 목록에서 주문 내역 삭제 하기
+    @DeleteMapping("/list/{orderId}")
+    public ResponseEntity<?> deleteOneInOrderList(
+            @PathVariable String orderId
+    ) {
+        String response = orderService.deleteOneInOrderList(orderId);
+        return ResponseEntity.ok(response);
+
     }
 }
