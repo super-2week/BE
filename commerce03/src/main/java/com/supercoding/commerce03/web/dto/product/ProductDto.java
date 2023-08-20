a75f4f96c05f0b1a1aeca849290d0f2db0b26535
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-08-20 15:11:06 +0900

    Feat: 모든 상품 응답시 관심상품 등록여부 체크 및 표시 + entity 칼럼추가 + 기타 리팩토링

--- commerce03/src/main/java/com/supercoding/commerce03/web/dto/product/ProductDto.java
+++ commerce03/src/main/java/com/supercoding/commerce03/web/dto/product/ProductDto.java
@@ -1,8 +1,6 @@
 package com.supercoding.commerce03.web.dto.product;
 
-import com.supercoding.commerce03.web.dto.product.util.AnimalCategory;
-import com.supercoding.commerce03.web.dto.product.util.ProductCategory;
-import com.supercoding.commerce03.web.dto.product.util.SmallCategory;
+import com.supercoding.commerce03.repository.store.entity.Store;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,13 +8,15 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDto {
     private Long id;
     private String imageUrl;
-    private Integer animalCategory;
-    private Integer productCategory;
-    private String productName;
+    private Integer animalCategory; //DB접근용도
+    private Integer productCategory; //DB접근용도
     private String storeName;
+    private String productName;
     private String modelNum;
     private String originLabel;
     private Integer price;
@@ -26,14 +26,15 @@ public class ProductDto {
     private Integer purchaseCount;
     private LocalDateTime createdAt;
     private boolean isLiked;
+    private Store store;
 
-    public ProductDto(Long id, String imageUrl, Integer animalCategory, Integer productCategory, String productName, String storeName, String modelNum, String originLabel, Integer price, String description, Integer stock, Integer wishCount, Integer purchaseCount, LocalDateTime createdAt) {
+    //DB 접근용 생성자
+    public ProductDto(Long id, String imageUrl, Integer animalCategory, Integer productCategory, String productName, String modelNum, String originLabel, Integer price, String description, Integer stock, Integer wishCount, Integer purchaseCount, LocalDateTime createdAt, Store store) {
         this.id = id;
         this.imageUrl = imageUrl;
         this.animalCategory = animalCategory;
         this.productCategory = productCategory;
         this.productName = productName;
-        this.storeName = storeName;
         this.modelNum = modelNum;
         this.originLabel = originLabel;
         this.price = price;
@@ -42,5 +43,7 @@ public class ProductDto {
         this.wishCount = wishCount;
         this.purchaseCount = purchaseCount;
         this.createdAt = createdAt;
+        this.store = store;
     }
+
 }
