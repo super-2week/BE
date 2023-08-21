91b49cefb441319e2c02a036e851538f7c4182f7
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-22 01:53:07 +0900

    Feat : S3 연동 및 이미지 저장, 삭제, 업데이트 등 기능 구현

--- commerce03/src/main/java/com/supercoding/commerce03/service/review/ReviewService.java
+++ commerce03/src/main/java/com/supercoding/commerce03/service/review/ReviewService.java
@@ -1,26 +1,31 @@
 package com.supercoding.commerce03.service.review;
 
+import static com.supercoding.commerce03.repository.review.entity.Review.toEntity;
+
 import com.supercoding.commerce03.repository.order.OrderRepository;
 import com.supercoding.commerce03.repository.product.ProductRepository;
 import com.supercoding.commerce03.repository.product.entity.Product;
+import com.supercoding.commerce03.repository.review.ReviewImageRepository;
 import com.supercoding.commerce03.repository.review.ReviewRepository;
 import com.supercoding.commerce03.repository.review.entity.Review;
+import com.supercoding.commerce03.repository.review.entity.ReviewImage;
 import com.supercoding.commerce03.repository.user.UserRepository;
 import com.supercoding.commerce03.repository.user.entity.User;
+import com.supercoding.commerce03.service.S3.S3Service;
 import com.supercoding.commerce03.service.review.exception.ReviewErrorCode;
 import com.supercoding.commerce03.service.review.exception.ReviewException;
 import com.supercoding.commerce03.web.dto.review.CreateReview;
 import com.supercoding.commerce03.web.dto.review.ModifyReview.Request;
 import com.supercoding.commerce03.web.dto.review.ReviewDto;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.relational.core.sql.render.RenderNamingStrategy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,10 +35,13 @@ public class ReviewService {
 	private final UserRepository userRepository;
 	private final ProductRepository productRepository;
 	private final ReviewRepository reviewRepository;
+	private final ReviewImageRepository reviewImageRepository;
 	private final OrderRepository orderRepository;
+	private final S3Service s3Service;
 
 	@Transactional
-	public ReviewDto createReview(CreateReview.Request request, Long userId){
+	public ReviewDto createReview(CreateReview.Request request, Long userId,
+			List<MultipartFile> multipartFile){
 
 		Long inputProductId = request.getProductId();
 
@@ -45,22 +53,17 @@ public class ReviewService {
 			throw new ReviewException(ReviewErrorCode.REVIEW_ALREADY_EXISTS);
 		}
 
-		return ReviewDto.fromEntity(
-				reviewRepository.save(
-						Review.builder()
-								.user(validatedUser)
-								.product(validatedProduct)
-								.title(request.getTitle())
-								.content(request.getContent())
-								.createAt(LocalDateTime.now())
-								.isDeleted(false)
-								.build()
-				)
-		);
+		Review review = toEntity(validatedUser, validatedProduct, request);
+		reviewRepository.save(review);
+
+		List<String> imageUrls = s3Service.uploadFiles(multipartFile);
+		saveReviewImage(review, imageUrls);
+
+		return ReviewDto.fromEntity(review);
 	}
 
 	@Transactional
-	public ReviewDto modifyReview(Request request, Long userId){
+	public ReviewDto modifyReview(Request request, Long userId, List<MultipartFile> multipartFile){
 
 		Long inputReviewId = request.getReviewId();
 
@@ -73,6 +76,10 @@ public class ReviewService {
 
 		validateReview.setTitle(request.getTitle());
 		validateReview.setContent(request.getContent());
+
+		deleteReviewImage(validateReview);
+		List<String> imageUrls = s3Service.uploadFiles(multipartFile);
+		saveReviewImage(validateReview, imageUrls);
 
 		return ReviewDto.fromEntity(validateReview);
 	}
@@ -87,6 +94,7 @@ public class ReviewService {
 			throw new ReviewException(ReviewErrorCode.NO_PERMISSION_TO_DELETE);
 		}
 
+		deleteReviewImage(validateReview);
 		validateReview.setIsDeleted(true);
 
 		return ReviewDto.fromEntity(validateReview);
@@ -134,5 +142,23 @@ public class ReviewService {
 
 	private boolean isNotReviewer(Long userId, Review review){
 		return !Objects.equals(userId, review.getUser().getId());
+	}
+
+	private void saveReviewImage(Review review, List<String> imageUrls){
+		for (String imageUrl : imageUrls) {
+			reviewImageRepository.save(
+					ReviewImage.builder()
+							.review(review)
+							.reviewImageUrl(imageUrl)
+							.build());
+		}
+	}
+
+	private void deleteReviewImage(Review review){
+		List<ReviewImage> reviewImages = review.getReviewImages();
+		for (ReviewImage reviewImage : reviewImages) {
+			s3Service.deleteFile(reviewImage.getReviewImageUrl());
+			reviewImageRepository.delete(reviewImage);
+		}
 	}
 }
