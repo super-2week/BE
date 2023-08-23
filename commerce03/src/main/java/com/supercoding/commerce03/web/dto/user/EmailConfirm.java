a397420aff6f4ee7bdc8264fda24f951c51eff28
jungryuel <zldrktduf0927@naver.com>
2023-08-24 01:32:52 +0900

    Fix: 요청값을 DTO를 사용해서 바꿨습니다 @RequestParam에서 @RequestBody로 바꿨습니다

---
+++ commerce03/src/main/java/com/supercoding/commerce03/web/dto/user/EmailConfirm.java
@@ -0,0 +1,17 @@
+package com.supercoding.commerce03.web.dto.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailConfirm {
+
+    private String email;
+
+}
