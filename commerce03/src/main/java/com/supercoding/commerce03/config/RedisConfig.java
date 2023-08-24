79418ad30e40e3a7c9c5416d46fd44619102507e
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-25 07:00:50 +0900

    Refactor : Redis Rollback 구현 및 네트워크 병목 개선

--- commerce03/src/main/java/com/supercoding/commerce03/config/RedisConfig.java
+++ commerce03/src/main/java/com/supercoding/commerce03/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.supercoding.commerce03.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,6 +13,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(redisHost);
+		redisStandaloneConfiguration.setPort(redisPort);
+
+		return new LettuceConnectionFactory(
+				redisStandaloneConfiguration);
+	}
 
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(
@@ -22,6 +41,7 @@ public class RedisConfig {
 		redisTemplate.setValueSerializer(serializer);
 		redisTemplate.setHashKeySerializer(serializer);
 		redisTemplate.setHashValueSerializer(serializer);
+
 		return redisTemplate;
 	}
 }
--- commerce03/src/main/java/com/supercoding/commerce03/service/cart/BasketService.java
+++ commerce03/src/main/java/com/supercoding/commerce03/service/cart/BasketService.java
@@ -5,14 +5,23 @@ import com.supercoding.commerce03.repository.product.ProductRepository;
 import com.supercoding.commerce03.repository.product.entity.Product;
 import com.supercoding.commerce03.repository.redis.Basket;
 import com.supercoding.commerce03.repository.redis.Basket.Item;
+import com.supercoding.commerce03.repository.user.UserRepository;
+import com.supercoding.commerce03.repository.user.entity.User;
 import com.supercoding.commerce03.service.cart.exception.CartErrorCode;
 import com.supercoding.commerce03.service.cart.exception.CartException;
 import com.supercoding.commerce03.web.dto.cart.AddCart;
 import com.supercoding.commerce03.web.dto.cart.ModifyCart.Request;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @Slf4j
@@ -20,7 +29,9 @@ import org.springframework.stereotype.Service;
 public class BasketService {
 
 	private final RedisClient redisClient;
+	private final RedisTemplate<String, Object> redisTemplate;
 	private final ProductRepository productRepository;
+	private final UserRepository userRepository;
 
 	public Basket getBasket(Long userId){
 		Basket basket = redisClient.get(userId, Basket.class);
@@ -29,7 +40,12 @@ public class BasketService {
 
 	public Basket addBasket(Long userId, AddCart.Request request){
 
-		Product validatedProduct = validateProduct(request.getProductId());
+		Long inputProductId = request.getProductId();
+		Integer inputQuantity = request.getQuantity();
+
+		validateUser(userId);
+		Product validatedProduct = validateProduct(inputProductId);
+		validateQuantity(inputQuantity, validatedProduct);
 
 		Basket basket = redisClient.get(userId, Basket.class);
 		if (basket == null) {
@@ -38,7 +54,7 @@ public class BasketService {
 		}
 
 		Optional<Item> optionalItem = basket.getProducts().stream()
-				.filter(p -> p.getProductId().equals(request.getProductId()))
+				.filter(p -> p.getProductId().equals(inputProductId))
 				.findFirst();
 
 		if (optionalItem.isPresent()) {
@@ -83,6 +99,45 @@ public class BasketService {
 		return basket;
 	}
 
+	public void insertItemList(Long userId, List<Item> itemList){
+
+		final String key = userId.toString();
+
+		RedisSerializer<String> keySerializer = redisTemplate.getStringSerializer();
+		RedisSerializer valueSerializer = redisTemplate.getValueSerializer();
+
+		redisTemplate.executePipelined((RedisCallback<Object>) RedisConnection -> {
+			itemList.forEach(item -> {
+				String Key = item.getProductId().toString();
+				String Value = valueSerializer.serialize(item).toString();
+
+				RedisConnection.hSet(keySerializer.serialize(key),
+						keySerializer.serialize(Key),
+						valueSerializer.serialize(Value)
+				);
+			});
+			return null;
+		});
+	}
+
+	public void restoreBasketListOnOrderRollback(Long userId, List<Item> itemList) {
+		TransactionSynchronizationManager.registerSynchronization(
+				new TransactionSynchronization() {
+					@Override
+					public void afterCompletion(int status) {
+						if (status == STATUS_ROLLED_BACK) {
+							insertItemList(userId, itemList);
+						}
+					}
+				});
+	}
+
+	private User validateUser(Long userId){
+		return userRepository.findById(userId)
+				.orElseThrow(() -> new CartException(CartErrorCode.USER_NOT_FOUND));
+	}
+
+
 	private Product validateProduct(Long productId){
 		Product product = productRepository.findById(productId)
 				.orElseThrow(() -> new CartException(CartErrorCode.THIS_PRODUCT_DOES_NOT_EXIST));
@@ -93,5 +148,15 @@ public class BasketService {
 		}
 
 		return product;
+	}
+
+	private void validateQuantity(Integer inputQuantity, Product product){
+		if (inputQuantity <= 0 || inputQuantity > product.getStock()) {
+			throw new CartException(CartErrorCode.INVALID_QUANTITY);
+		}
+	}
+
+	private void run(Long userId, List<Item> itemList) {
+		restoreBasketListOnOrderRollback(userId, itemList);
 	}
 }
